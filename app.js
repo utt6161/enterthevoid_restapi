5070c107dffbc53d75d4a83c85faf12bcc9e6ddf
ut61a <utt6161@yandex.ru>
2019-02-26 20:48:54 +0500

    nothing to commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+  });
+  
 
