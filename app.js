4dfeb353fa73dc60a4c138540af86727cbf48d4d
Denis Zuev <45436035+utt6161@users.noreply.github.com>
2019-02-27 09:46:31 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "u615qyjzybll9lrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  user: "iyvj67ll5omfb7dy",
+  password: "pedbphao75mx0qu5",
+  database: "cyjd3gv7n57trq48"
+});
+
 app.get('/', function (req, res) {
-  res.send('hello world!');
+  con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT name, address FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    res.send(fields);
+  });
+});
 });
 
 app.listen(port, "0.0.0.0", function() {
