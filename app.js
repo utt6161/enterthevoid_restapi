8125543365e5ec7ac0a35cefa3c0bf2fc70803a6
Denis Zuev <45436035+utt6161@users.noreply.github.com>
2019-02-27 11:14:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, "0.0.0.0", function() {
-  console.log("Listening on Port " + post );
+  console.log("Listening on Port " + port );
   });
   
 
