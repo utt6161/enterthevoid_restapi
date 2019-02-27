f3c5e04b37e75f11938975179655e0329f8b7ebe
Denis Zuev <45436035+utt6161@users.noreply.github.com>
2019-02-27 09:23:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('GACHI.COMPANY');
+  res.send('hello world!');
 });
 
 app.listen(port, "0.0.0.0", function() {
