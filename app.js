51d5431fef1ce6cf38ede471d6ecd50dec193135
Denis Zuev <45436035+utt6161@users.noreply.github.com>
2019-02-27 10:56:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.get('/', function (req, res) {
   con.connect(function(err) {
   if (err) throw err;
   con.query("SELECT * FROM *", function (err, result, fields) {
-    if (err) throw err;
     res.send(fields);
   });
 });
