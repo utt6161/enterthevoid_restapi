36011b5a9e2f9edcfc82f46698ad9d73983c798a
ut61a <utt6161@yandex.ru>
2019-02-27 19:42:48 +0500

    kk

--- app.js
+++ app.js
@@ -16,41 +16,45 @@ app.use(express.json());         // to support JSON-encoded bodies
 app.get('/', function (req, res) {
   con.connect(function(err) {
   if (err) throw err;
-  // con.query("SELECT * FROM scores ORDER BY value DESC", function (err, result, fields) {
-  //   res.send(result);
-  // });
-
-  con.query("SELECT COUNT(*) FROM scores", function (err, result, fields) {
+  con.query("SELECT * FROM scores ORDER BY value DESC", function (err, result, fields) {
     res.send(result);
   });
 
+  // con.query("SELECT COUNT(*) FROM scores", function (err, result, fields) {
+  //   res.send(result);
+  // });
+
 });
 });
 
-// app.post('/', function(req, res){
-//   var name = req.body.name,
-//       value = req.body.value;
-//       id = req.body.id;
-//   con.connect(function(err) {
-//     if (err) throw err;
-//     con.query("SELECT COUNT(*) FROM scores", function (err, result, fields) {
-//       if(id!=0){
-//         con.query("UPDATE scores SET name = " + name + ", value = " + value + "WHERE id = " + id, function (err, result, fields) {
-//           if (err) throw err;
-//           console.log("1 record inserted");
-//         });
-//       }
-//       else{
-//         con.query("INSERT INTO scores (name, value) VALUES ('"+ name + "', '" + value + "')", function (err, result, fields) {
-//           if (err) throw err;
-//           console.log("1 record inserted");
-//         });
-//       }
-
-//     });
+app.post('/', function(req, res){
+  var name = req.body.name,
+      value = req.body.value;
+      id = req.body.id;
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT COUNT(*) FROM scores", function (err, result, fields) {
+      if((result[0][0] == 10) && (id == 0)){ // result of COUNT(*)
+        res.send(0);
+      }
+      else{
+        if(id!=0){
+          con.query("UPDATE scores SET name = " + name + ", value = " + value + "WHERE id = " + id, function (err, result, fields) {
+            if (err) throw err;
+            console.log("1 record updated");
+          });
+        }
+        else{
+          con.query("INSERT INTO scores (name, value) VALUES ('"+ name + "', '" + value + "')", function (err, result, fields) {
+            if (err) throw err;
+            console.log("1 record inserted");
+          });
+        }
+      }
+    });
   
-//   });
-// });
+  });
+});
 
 app.listen(port, "0.0.0.0", function() {
   console.log("Listening on Port " + port );
