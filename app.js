9950658ee83b5de5c7003ba1508fa8dc9ab2d167
Denis Zuev <45436035+utt6161@users.noreply.github.com>
2019-02-27 11:14:16 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(port, "0.0.0.0", function() {
-  console.log("Listening on Port 3000");
+  console.log("Listening on Port " + post );
   });
   
 
